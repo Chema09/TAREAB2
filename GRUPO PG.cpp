11acde0a515333d3048640877dd700d0b1ed9408
CONTRERASLUIS <86172235+CONTRERASLUIS@users.noreply.github.com>
2021-06-23 10:22:22 -0500

    Update GRUPO PG.cpp
    agrege los elementos a la matriz para realizar las sumas de las filas y columnas de las matriz1 y matriz2

--- GRUPO PG.cpp
+++ GRUPO PG.cpp
@@ -47,8 +47,49 @@ int main(){
 						 system("pause>nul"); // Pausa 
 						break;
 					}
-		            	
-		                
+		       
+
+//PARTE DE CONTRERAS MONTAÑO LUIS MIGUEL
+
+                case 2:{
+                int filas2, columnas2;
+                cout<<"Digite el numero de filas: ";
+                cin>>filas2;
+                cout<<"Digite el numero de columnas: ";
+                cin>>columnas2;
+
+                int A[filas2][columnas2],B[filas2][columnas2],C[filas2][columnas2];
+     //Almacenamos elementos en la matriz
+                cout<<end1<<"MATRIZ 1 :";
+                for(int i=0;i<filas2;i++){
+                for(int j=0;j<columnas2;j++){
+                cout<<"Digite un numero en la MATRIZ 1 posicion ["<<i<<"]["<<j<<"]: ";
+                cin>>A[i][j];
+                    }
+                }
+
+                cout<<end1<<"MATRIZ 2 :";
+                for(int i=0;i<filas2;i++){
+                for(int j=0;j<columnas2;j++){
+                cout<<"Digite un numero en la MATRIZ 2 posicion ["<<i<<"]["<<j<<"]: ";
+                cin>>B[i][j];
+                    }
+                }
+     
+     //PROCESO DE SUMA
+                for(int i=0;i<filas2;i++){
+                for(int j=0;j<columnas;j++){
+                C[i][j]=A[i][j]+B[i][j];
+                    }
+                }
+ 
+                for(int i=0;i<filas2;i++){
+                for(int j=0;j<columnas2;j++){
+                cout<<end1;
+                    }
+                system("pause>nul"); // Pausa
+                break;
+          }		                
 		            
 						
 		            	
